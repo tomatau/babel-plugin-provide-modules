1545cfe3746bb57a7d05294749436e73f2e8c642
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2015-09-27 10:52:58 +0100

    added test file and running it

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+import babel from 'babel-core';
+import {expect} from 'chai';
+import providePlugin from '../src';
+
+describe('Test',()=>{
+  it('shuld test',()=>{
+    expect('this').to.eql('this')
+  });
+});
